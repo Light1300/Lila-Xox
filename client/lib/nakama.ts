4cf0e8c944c908e7ef68830e7f0c9a2e08d70207
Sarvesh Patil <sarupatil0001@gmail.com>
2026-04-06 00:05:28 +0530

    Edit for prod

--- client/lib/nakama.ts
+++ client/lib/nakama.ts
@@ -15,7 +15,7 @@ class Nakama {
 
     constructor() {
         this.client = new Client(
-            "defaultkey",
+            "xvo-nakama-prod-2026",
             process.env.NEXT_PUBLIC_SERVER_API,
             process.env.NEXT_PUBLIC_SERVER_PORT,
             process.env.NEXT_PUBLIC_USE_SSL === "true"
